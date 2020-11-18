f75bf276610f35643c04a47c17edd0c3b70c6176
lengqi <1982182894@qq.com>
2020-11-18 22:55:08 +0800

    创建认证服务器（基于jdbc存储令牌）

--- test-security-oauth-server/src/main/java/com/lengqi/oauth/config/AuthorizationServerConfiguration.java
+++ test-security-oauth-server/src/main/java/com/lengqi/oauth/config/AuthorizationServerConfiguration.java
@@ -5,16 +5,22 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableAuthorizationServer
@@ -66,10 +72,16 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
         return new JdbcTokenStore(dataSource());
     }
 
+    @Resource
+    private AuthenticationManager authenticationManager;
+
+
+
     /**
      * 基于jdbc实现，需要事先在数据库配置客户端信息
      * @return
      */
+    /**
     public JdbcClientDetailsService clientDetailsService(){
         return new JdbcClientDetailsService(dataSource());
     }
@@ -77,11 +89,42 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        // 设置令牌
         endpoints.tokenStore(tokenStore());
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        //读取客户端配置
         clients.withClientDetails(clientDetailsService());
+    }
+     */
+    /**
+     * 基于jwt存储令牌，不用保存，一般设置时间
+     * @param
+     * @throws Exception
+     */
+
+    //使用同一个密钥来编码 JWT 中的  OAuth2 令牌
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("123");
+        return converter;
+    }
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                // 使用in-memory存储客户端信息
+                .inMemory()
+                //client_id
+                .withClient("client")
+                //client_secret
+                .secret("secret")
+                //授权类型
+                .authorizedGrantTypes("authorization_code")
+                //授权范围
+                .scopes("app")
+                .autoApprove(true) ;//登录后绕过批准询问(/oauth/confirm_access)
     }
 }
